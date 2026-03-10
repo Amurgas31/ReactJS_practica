1a427d84f3c281cdffe0f257ff488981a2cf4fba
Amurgas31 <20240235@ricaldone.edu.sv>
2026-03-10 17:21:21 -0600

    Ejercicio 4

--- ejercicio-4/src/App.jsx
+++ ejercicio-4/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   // Definimos el arreglo de datos
   const listaUsuarios = [
-    { id: 1, nombre: "Juan Pérez", edad: 28, profesion: "Ingeniero", ciudad: "San Salvador", imagen: "https://randomuser.me/api/portraits/men/99.jpg" },
+    { id: 1, nombre: "Juanito Pérez", edad: 28, profesion: "Ingeniero", ciudad: "San Salvador", imagen: "https://randomuser.me/api/portraits/men/99.jpg" },
     { id: 2, nombre: "María López", edad: 25, profesion: "Diseñadora", ciudad: "Santa Tecla", imagen: "https://randomuser.me/api/portraits/women/2.jpg" },
     { id: 3, nombre: "Carlos Ruiz", edad: 32, profesion: "Chef", ciudad: "Santa Ana", imagen: "https://randomuser.me/api/portraits/men/79.jpg" },
     { id: 4, nombre: "Ana Martínez", edad: 29, profesion: "Doctora", ciudad: "San Miguel", imagen: "https://randomuser.me/api/portraits/women/17.jpg" }
